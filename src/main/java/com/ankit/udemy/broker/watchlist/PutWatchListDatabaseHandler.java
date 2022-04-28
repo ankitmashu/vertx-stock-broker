2c10984b98c2f8040c37e0473113791f4c6dc25d
Ankit Singh <ankitmashu@gmail.com>
2022-04-28 10:45:34 +0530

    26/04/2022

--- src/main/java/com/ankit/udemy/broker/watchlist/PutWatchListDatabaseHandler.java
+++ src/main/java/com/ankit/udemy/broker/watchlist/PutWatchListDatabaseHandler.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PutWatchListDatabaseHandler implements Handler<RoutingContext> {
  private static final Logger LOG= LoggerFactory.getLogger(PutWatchListDatabaseHandler.class);
@@ -26,22 +27,23 @@ public class PutWatchListDatabaseHandler implements Handler<RoutingContext> {
     var json = context.getBodyAsJson();
     var watchList= json.mapTo(WatchList.class);
 
-    watchList.getAssets().forEach(asset -> {
-      final HashMap<String, Object> parameters= new HashMap<>();
-      parameters.put("account_id",accountId);
-      parameters.put("asset",asset.getName());
+     var parameterBatch= watchList.getAssets().stream()
+        .map(asset -> {
+          final Map<String, Object> parameters= new HashMap<>();
+          parameters.put("account_id",accountId);
+          parameters.put("asset",asset.getName());
+          return  parameters;
+        }).collect(Collectors.toList());
+//only adding is possible -> Enteries for watch list never removed
       SqlTemplate.forUpdate(db,
-          "INSERT INTO broker.watchlist VALUES (#{account_id},#{asset})")
-        .execute(parameters)
+          "INSERT INTO broker.watchlist VALUES (#{account_id},#{asset})" +
+          " ON CONFLICT (account_id, asset) DO NOTHING")
+        .executeBatch(parameterBatch)
         .onFailure(DbResponse.errorHandler(context,"Failed to insert into watchlist"))
-        .onSuccess(result->{
-          if(!context.response().ended()) {
+        .onSuccess(result->
             context.response()
               .setStatusCode(HttpResponseStatus.NO_CONTENT.code())
-              .end();
-          }
-        });
-    });
-
+              .end()
+        );
   }
 }
